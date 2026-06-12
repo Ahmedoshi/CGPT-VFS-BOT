84e8fcfbb5b21ba4c55fd2c43e33205802d945a0
Ahmed A Oshi <cosmos@Ahmeds-MacBook-Air.local>
2026-06-12 18:15:05 +0300

    real email

--- index.js
+++ index.js
@@ -4,37 +4,61 @@ const URL =
   "https://lift-api.vfsglobal.com/appointment/CheckIsSlotAvailable";
 
 async function check(name, visaCategoryCode) {
+
   try {
-    const response = await axios.post(URL, {
-      countryCode: "SAU",
-      missionCode: "PRT",
-      vacCode: "PORRIY",
-      visaCategoryCode,
-      roleName: "Individual",
-      loginUser: "test@test.com",
-      payCode: ""
-    });
+
+    const response = await axios.post(
+      URL,
+      {
+        countryCode: "SAU",
+        missionCode: "PRT",
+        vacCode: "PORRIY",
+        visaCategoryCode,
+        roleName: "Individual",
+        loginUser: "oshitdamerit2021@gmail.com",
+        payCode: ""
+      }
+    );
 
     console.log(
       new Date().toISOString(),
       name,
       response.status
     );
+
   } catch (e) {
+
     console.log(
       new Date().toISOString(),
       name,
       e.response?.status || "ERROR"
     );
+
   }
+
 }
 
 async function run() {
-  await check("D2", "LSVRVFS");
-  await check("D3", "LSTVRVF");
-  console.log("--------------------------------");
+
+  await check(
+    "D2",
+    "LSVRVFS"
+  );
+
+  await check(
+    "D3",
+    "LSTVRVF"
+  );
+
+  console.log(
+    "--------------------------------"
+  );
+
 }
 
 run();
 
-setInterval(run, 60000);
+setInterval(
+  run,
+  60000
+);
